ca5ada123bcbf97c9969f0ec1a4c51294903e6e0
Deepak Subramanian <subudeepak@gmail.com>
2013-03-19 20:18:53 +0100

    This is the first commit

---
+++ iOSProTables/iOSProTables/ProTablesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ProTablesAppDelegate.h
+//  iOSProTables
+//
+//  Created by Deepak Subramanian on 19/3/13.
+//  Copyright (c) 2013 Deepak Subramanian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProTablesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

d394661970857bf2cf9fe5b77378b18f1f90157b
Deepak Subramanian <subudeepak@gmail.com>
2013-03-19 20:20:15 +0100

    This is the first commit
    The files in the master are still blank

---
+++ iOSProTables/iOSProTables/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSProTables
+//
+//  Created by Deepak Subramanian on 19/3/13.
+//  Copyright (c) 2013 Deepak Subramanian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ProTablesAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ProTablesAppDelegate class]));
+    }
+}
